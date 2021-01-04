aa5a08e13102b1ee30e2e258bfdc5447ace3e2ab
duan-le <56768212+duan-le@users.noreply.github.com>
2021-01-03 22:15:27 -0700

    Implement write to csv.

--- webscraper/uofc_scraper.py
+++ webscraper/uofc_scraper.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 r = requests.get('https://ucalgary.ca/risk/emergency-management/covid-19-response/covid-19-dashboard')
@@ -8,6 +9,21 @@ results = soup.find_all('div', {'class': 'layout-blocks-ucws-text container-flui
 results = results[2].find_all('div', {'class': 'col-sm-12 one-col'})
 results = results[0].find_all('p')
 
-for p in results:
-    new_string = p.text.replace('notification', '').replace('Read more.', '').replace('Read More.', '').replace(u'\xa0', ' ').strip()
-    print(new_string)
+with open('uofc_covid_cases.csv', 'w') as csv_file:
+    writer  = csv.writer(csv_file)
+    columns = ['Date', 'Location']
+    writer.writerow(columns)
+    
+    for p in results:
+        if p.find('strong') != None:
+            p.strong.decompose()
+        
+        rows = p.get_text().strip().replace('notification:', '|').replace('Read more.', '').replace('Read More.', '').replace(u'\xa0', ' ').split('\n')
+    
+        for row in rows:
+            row = row.split('|')
+            if len(row) == 2:
+                row[0] = row[0].strip()
+                row[1] = row[1].strip().strip('.')
+                writer.writerow(row)
+                print(row)
