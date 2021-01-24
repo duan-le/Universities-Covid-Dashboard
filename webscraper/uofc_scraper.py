1da04ad5b976587252655145fede2e191f036f2f
duan-le <56768212+duan-le@users.noreply.github.com>
2021-01-23 17:10:06 -0700

    Semi working U of C scraper

--- webscraper/uofc_scraper.py
+++ webscraper/uofc_scraper.py
@@ -1,22 +1,22 @@
 import requests
-import csv
 import mysql.connector
+import csv
 from bs4 import BeautifulSoup
 
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
     password = "password",
-    database = "covid_database"
+    database = "university_covid"
 )
 
 mycursor = mydb.cursor()
 
-r = requests.get('https://www.ucalgary.ca/risk/emergency-management/covid-19-response/covid-19-dashboard/past-cases')
+r = requests.get('https://ucalgary.ca/risk/emergency-management/covid-19-response/covid-19-dashboard/past-cases')
 
 soup = BeautifulSoup(r.text, 'html.parser')
 results = soup.find_all('div', {'class': 'layout-blocks-ucws-text container-fluid roundable block text'})
-results = results[2].find_all('div', {'class': 'col-sm-12 one-col'})
+results = results[0].find_all('div', {'class': 'col-sm-12 one-col'})
 results = results[0].find_all('p')
 
 with open('uofc_covid_cases.csv', 'w', newline='') as csv_file:
@@ -35,9 +35,37 @@ with open('uofc_covid_cases.csv', 'w', newline='') as csv_file:
             if len(row) == 2:
                 row[0] = row[0].strip()
                 row[1] = row[1].strip().strip('.')
-                # sql = "INSERT INTO uofc (uni_Name, cdate, cases) VALUES (%s, %s, %s)"
-                # val = ("University of Calgary", str(row[0]), 1)
-                # mycursor.execute(sql, val)
-                # mydb.commit()
+                sql = "INSERT INTO alberta (university_name, date_range, cases) VALUES (%s, %s, %s)"
+                val = ("University of Calgary", str(row[0]), str(1))
+                mycursor.execute(sql, val)
+                mydb.commit()
+                writer.writerow(row)
+                print(row)
+
+r = requests.get('https://ucalgary.ca/risk/emergency-management/covid-19-response/covid-19-dashboard')
+
+soup = BeautifulSoup(r.text, 'html.parser')
+results = soup.find_all('div', {'class': 'layout-blocks-ucws-text container-fluid roundable block text'})
+results = results[2].find_all('div', {'class': 'col-sm-12 one-col'})
+results = results[0].find_all('p')
+
+with open('uofc_covid_cases.csv', 'a', newline='') as csv_file:
+    writer  = csv.writer(csv_file)
+
+    for p in results:
+        if p.find('strong') != None:
+            p.strong.decompose()
+        
+        rows = p.get_text().strip().replace('notification:', '|').replace('Read more.', '').replace('Read More.', '').replace(u'\xa0', ' ').replace('notification.', '|').split('\n')
+    
+        for row in rows:
+            row = row.split('|')
+            if len(row) == 2:
+                row[0] = row[0].strip()
+                row[1] = row[1].strip().strip('.')
+                sql = "INSERT INTO alberta (university_name, date_range, cases) VALUES (%s, %s, %s)"
+                val = ("University of Calgary", str(row[0]), str(1))
+                mycursor.execute(sql, val)
+                mydb.commit()
                 writer.writerow(row)
                 print(row)
