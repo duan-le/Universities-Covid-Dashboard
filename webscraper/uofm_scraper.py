99b3cab13e450fc7f00d5b872ce3b1df78524df3
duan-le <56768212+duan-le@users.noreply.github.com>
2021-01-23 23:45:12 -0700

    Update U of M scraper

--- webscraper/uofm_scraper.py
+++ webscraper/uofm_scraper.py
@@ -17,23 +17,26 @@ r = requests.get('https://umanitoba.ca/coronavirus/latest-updates-and-safety')
 soup = BeautifulSoup(r.text, 'html.parser')
 results = soup.find_all('table')
 
-with open('uofm_covid_cases.csv', 'w', newline='') as csv_file:
-    writer  = csv.writer(csv_file)
-    columns = ['Date Range', 'Number of Cases']
-    writer.writerow(columns)
+columns = ['Date Range', 'Number of Cases']
+print(columns)
+print("Added:")
 
-    row = []
-    i = 0
-    for p in results[0].find_all('td'):
-        if p.find('strong') == None:       
-            row.append(p.get_text().strip())
-            i += 1
-            if i == 2:
-                print(row)
-                sql = "INSERT INTO manitoba (university_name, date_range, cases) VALUES (%s, %s, %s)"
-                val = ("University of Manitoba", str(row[0]), str(row[1]))
+row = []
+i = 0
+for p in results[0].find_all('td'):
+    if p.find('strong') == None:       
+        row.append(p.get_text().strip())
+        i += 1
+        if i == 2:
+            sql = "INSERT INTO manitoba (university_name, date_range, cases) VALUES (%s, %s, %s)"
+            val = ("University of Manitoba", str(row[0]), str(row[1]))
+            try:
                 mycursor.execute(sql, val)
                 mydb.commit()
-                writer.writerow(row)
+            except:
                 row.clear()
                 i = 0
+                continue
+            print(row)
+            row.clear()
+            i = 0
