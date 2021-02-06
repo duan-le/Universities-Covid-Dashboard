820f623867f9fc7d8f80b0e4af9cd54a69d65df3
jason-szeto <jasonszeto10@gmail.com>
2021-02-05 18:01:48 -0700

    added favicons

--- src/universities/uoft/dashboard_uoft.php
+++ src/universities/uoft/dashboard_uoft.php
@@ -11,6 +11,8 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
   <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+  <link rel="icon" type="image/png" sizes="32x32" href="../assets/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="../assets/favicon/favicon-16x16.png">
 </head>
 
 <body>
